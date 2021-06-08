afa9224fd12308ad5398799bf6620b3563016a3b
Vu Thanh Tung <thanhtungvu.hust@gmail.com>
2021-06-08 19:02:17 +0700

    setup-debos: remove internal config
    Signed-off-by: Vu Thanh Tung <thanhtungvu.hust@gmail.com>

--- scripts/setup-debos.sh
+++ scripts/setup-debos.sh
@@ -47,23 +47,12 @@ sudo rm -rf /var/lib/apt/lists/*
 git config --global http.proxy "$http_proxy"
 git config --global https.proxy "$http_proxy"
 
-[ ! -f "/usr/share/ca-certificates/toshiba.cer" ] && wget -e http_proxy=$http_proxy -e https_proxy=$http_proxy --no-proxy http://www.iss.toshiba.co.jp/services/ss/ca/manual/certificate/toshiba.cer && mv toshiba.cer /usr/share/ca-certificates/
-[ ! -f "/usr/share/ca-certificates/toshibaca.cer" ] && wget -e http_proxy=$http_proxy -e https_proxy=$http_proxy --no-proxy http://www.iss.toshiba.co.jp/services/ss/ca/manual/certificate/toshibaca.cer && mv toshibaca.cer /usr/share/ca-certificates/
-
-if [[ -z $(grep "toshiba.cer" /etc/ca-certificates.conf) ]]; then
-	sudo echo toshiba.cer >> /etc/ca-certificates.conf
-fi
-if [[ -z $(grep "toshibaca.cer" /etc/ca-certificates.conf) ]]; then
-	sudo echo toshibaca.cer >> /etc/ca-certificates.conf
-fi
-sudo update-ca-certificates
-
 # Download debos
 if [ ! -d "$GOPATH/src/github.com/go-debos/debos" ]; then
 	 mkdir -p "$GOPATH/src/github.com/go-debos/debos" 
 fi
 cd "$GOPATH/src/github.com/go-debos/debos"
-[ -z "$(ls -A .)" ] && git clone https://tosspedia.swc.toshiba.co.jp/gitlab/skelios/debos/skelios-debos.git .
+[ -z "$(ls -A .)" ] && git clone https://github.com/go-debos/debos.git .
 
 # Build debos
 if [[ -z $(command -v debos) ]]; then
